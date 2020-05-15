11c9014ccf1fedefc1d2e1c778e3e60578f436e1
ProfesorDiegoRosado <profesor.diego.rosado@gmail.com>
2020-05-15 20:12:33 +0200

    Adding battleships game

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+# Repositorio de juegos en python
+
+La finalidad de este repositorio es tener una serie de juegos en python que se ejecuten principalmente sin entorno gráfico y cuyo objetivo principal es ser un recurso más para la enseñanza de programación python en bachiller.
+
+## Requisitos
+
+Los juegos están implementados utilizando python 3.6+ y haciendo uso del mínimo de librerías posibles para facilitar su configuración y puesta en marcha
+
+Por lo general se utiliza virtual environment de python para aislar los entornos de desarrollo pero no se un requisito. De hecho intentaremos subir el código fuente con las mínimias dependencias posibles.
+
+## El código fuente
+
+El código fuente estará escrito principalmente en inglés por tratarse del "idioma universal" del desarrollo software. No obstante, y con el fin en mente de que sea un recurso útil para la enseñanza de programación python en bachiller, puede que algunos comentarios explicatorios del código aparezcan en español.
+
+## Juegos
+
+Cada juego tiene su carpeta individual. Se podrá descargar y ejecutar de manera independendiente.
+Cada carpeta de juego incluirá su README.md independiente donde se tratarán las particularidades de cada juego (configuración, como ejecutarlo, como se juega, ...)
+
+El listado de juegos disponible es:
+ * Battleships (hundir la flota)
+
+
+
+
+# Licencia
+La licencia del código de este repositorio es MIT.
---
+++ battleships/src/main.py
@@ -0,0 +1,154 @@
+# Author: Profesor Diego Rosado
+# Battleships game python implementation
+
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+BLINK = "\033[33;5m"
+BLUE = "\033[34m"
+LIGHT_GRAY = "\033[37m"
+NORMAL = "\033[0m"
+
+BG_BLUE = "\033[104m"
+BG_YELLOW = "\033[103m"
+
+
+UNKNOWN: chr = '□'
+BATTLESHIP: chr = 'o'
+WATER: chr = '~'
+HIT: chr = '#'
+
+
+class Board:
+
+    def __init__(self):
+        """
+        self.hidden_board = [
+            #1           2           3           4           5        6           7        8
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # a
+            [WATER,      BATTLESHIP, BATTLESHIP, BATTLESHIP, WATER,      WATER,      WATER,      WATER],  # b
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # c
+            [BATTLESHIP, WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # d
+            [BATTLESHIP, WATER,      WATER,      WATER, BATTLESHIP, BATTLESHIP,      WATER,      WATER],  # e
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # f
+            [WATER,      WATER,      BATTLESHIP, WATER,      WATER,      WATER,      WATER,      WATER],  # g
+            [WATER,      WATER,      WATER,      WATER,      WATER, BATTLESHIP, BATTLESHIP, BATTLESHIP]   # h
+        ]
+        """
+        self.hidden_board = [
+            #1           2           3           4           5        6           7        8
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # a
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # b
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # c
+            [BATTLESHIP, WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # d
+            [BATTLESHIP, WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # e
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # f
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER],  # g
+            [WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER,      WATER]   # h
+        ]
+        self.board = [[UNKNOWN for row in range(len(self.hidden_board))] for column in range(len(self.hidden_board[0]))]
+
+    def draw(self):
+        print("  1 2 3 4 5 6 7 8")
+        for i in range(len(self.board)):
+            row = self.board[i]
+            print(chr(ord('a')+i) + " ", end="")
+            for j in range(len(row)):
+                tile = row[j]
+                print(tile + " ", end="")
+                #print(" x: " + str(i) + " , j: " + str(j), end="")
+            print()
+
+    def draw_cool(self, turn: (int,int)):
+        print("  1 2 3 4 5 6 7 8")
+        for i in range(len(self.board)):
+            row = self.board[i]
+            print(chr(ord('a')+i) + " ", end="")
+            for j in range(len(row)):
+                tile = row[j]
+                if i == turn[0] and j == turn[1]:
+                    #print(BLINK, end="")
+                    print(BG_YELLOW, end="")
+                elif tile == WATER:
+                    print(BLUE, end="")
+                    print(BG_BLUE, end="")
+                elif tile == HIT:
+                    print(LIGHT_GRAY, end="")
+                print(tile + " ", end="")
+                print(NORMAL, end="")
+                #print(" x: " + str(i) + " , j: " + str(j), end="")
+            print()
+
+
+    def check(self, turn: (int, int)):
+        row = turn[0]
+        column = turn[1]
+        spot = self.hidden_board[row][column]
+        if spot == WATER:
+            self.board[row][column] = WATER
+            return WATER
+        else:  # spot==BATTLESHIP
+            self.board[row][column] = HIT
+            return HIT
+
+    def count(self, board, symbol):
+        count = 0
+        for i in range(len(board)):
+            row = board[i]
+            for j in range(len(row)):
+                element = row[j]
+                if element == symbol:
+                    count = count + 1
+        return count
+
+    def check_end(self):
+        battleships = self.count(self.hidden_board, BATTLESHIP)
+        hits = self.count(self.board, HIT)
+        return battleships == hits
+
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def read_row(self):
+        row = input("Introduce una letra (entre a y h): ")
+        if 'a' <= row <= 'h':
+            row = ord(row) - ord('a')
+        else:
+            print("Introduzca una letra entre la a y la h")
+            return self.read_row()
+        return row
+
+    def read_column(self):
+        column = input("Introduce un numero (entre 1 y 8): ")
+        if '1' <= column <= '8':
+            column = ord(column) - ord('1')
+        else:
+            print("Introduzca un numero entre la 1 y 8")
+            return self.read_column()
+        return column
+
+    def read_turn(self):
+        row = self.read_row()
+        column = self.read_column()
+        return (row, column)
+
+
+end = False
+
+board = Board()
+player = Player()
+
+board.draw()
+
+while not end:
+    turn: (int,int) = player.read_turn()
+    result = board.check(turn)
+    if result == WATER:
+        print("WATER!!!!")
+    else:
+        print("HIT!!!!")
+    board.draw_cool(turn)
+    end = board.check_end()
+
